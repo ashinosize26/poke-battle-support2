ddcc28cd7b21ed9afa8bf6df5d242825d98d70d8
tt <ashinosize26@gmail.com>
2023-11-27 23:25:45 +0900

    [add] タイプ表を追加

---
+++ Model/PartyTypeEffectiveRowModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PokeBattleSupport2.Model;
+public class PartyTypeEffectiveRowModel
+{
+    public string TypeName { get; set; } = string.Empty;
+
+    public TypeEffectiveMarkModel[] Marks { get; set; } = new TypeEffectiveMarkModel[6];
+
+    public int SuperEffectiveCount { get; set; }
+
+    public int WeakAgainstCount { get; set; }
+}
